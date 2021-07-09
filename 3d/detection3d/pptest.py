18d96aec0e8aa00bce1fb961d0282ac5075dd2b0
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-09 08:09:27 +0000

    update

--- 3d/detection3d/pptest.py
+++ 3d/detection3d/pptest.py
@@ -8,7 +8,7 @@ from google.protobuf import text_format
 
 import re
 import inspect
-from collections import defaultdict, OrderedDict
+from collections import OrderedDict
 
 
 class Model(nn.Module):
@@ -17,19 +17,23 @@ class Model(nn.Module):
         model_params = pp.ModelParameter()
         text_format.Merge(open(config, 'rb').read(), model_params)
         
-            
-        self.model_params = model_params
         self.model = self.parse(model_params)
+
         
-    def forward(self, x):
-        pass
+    def forward(self, data):
+        
+        outputs = {}
+
     
-        
     def parse(self, config):
         '''parse
         '''
-        modules = []
+        modules = nn.ModuleList()
+        
+        outputs = []
+                
         for i, m in enumerate(config.module):
+            
             assert m.type in MODULES, ''
             _class = MODULES[m.type]
             
@@ -41,18 +45,19 @@ class Model(nn.Module):
                 kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
 
             _param = [name for name in dir(m) if '_param' in name.lower() and m.type.lower() in name.lower()]
-            assert len(_param) == 1, ''
-            param = getattr(m, _param[0])
-            param = {k.name: v for k, v in param.ListFields()}
+            assert len(_param) == 1, f'must define {m.type.lower()}_param'
+            
+            param = {k.name: v for k, v in getattr(m, _param[0]).ListFields()}
             
             kwargs.update({k: param[k] for k in argsname if k in param})
-            
             kwargs = OrderedDict([(k, kwargs[k]) for k in argsname])
             
             modules.append( _class(**kwargs) )
+            
             
         return modules
     
     
 model = Model()
+
 print(model)
