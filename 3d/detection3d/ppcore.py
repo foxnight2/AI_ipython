0dfc96b5a87c50e244cffb54f0233578aff39b50
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-09 04:22:25 +0000

    update

---
+++ 3d/detection3d/ppcore.py
@@ -0,0 +1,31 @@
+import torch
+import torch.nn as nn 
+
+
+
+
+class Conv2d(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel, stride=1, padding=0, ):
+        super().__init__()
+
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel, stride, )
+        
+    def forward(self, x):
+        
+        x = self.conv(x)
+        
+        return x
+    
+    
+class ResNet(nn.Module):
+    def __init__(self, layers):
+        super().__init__()
+    
+    def forward(self, x):
+        pass
+    
+    
+modules = {
+    'Conv2d': Conv2d,
+    'ResNet': ResNet
+}
