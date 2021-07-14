57aa0934f0328b08effcd8150b85d2e1966cb966
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-14 16:48:13 +0800

    update

--- pp/protobuf/pptest.py
+++ pp/protobuf/pptest.py
@@ -16,6 +16,152 @@ import collections
 from typing import Sequence
 
 
+
+
+
+# parse module
+def _parse_module(config, classes):
+    '''instantiate a module
+    '''
+    if config.type == 'Custom':
+        _code = config.custom_param.module_inline if config.custom_param.module_inline \
+            else open(config.custom_param.module_file, 'r').read()
+        exec(_code)
+
+        return locals()[config.name]
+
+    _type = config.type
+    _class = classes[_type]
+
+    argspec = inspect.getfullargspec(_class.__init__)
+    argsname = [arg for arg in argspec.args if arg != 'self']
+
+    kwargs = {}
+
+    if argspec.defaults is not None:
+        kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
+
+    if hasattr(config, f'{_type.lower()}_param'):
+        _param = getattr(config, f'{_type.lower()}_param')
+        _param = {k.name: v for k, v in _param.ListFields()}
+        _param.update({k: (list(v)[0] if len(v) == 1 else list(v)) \
+                      for k, v in _param.items() if isinstance(v, pyext._message.RepeatedScalarContainer)})
+
+        kwargs.update({k: _param[k] for k in argsname if k in _param})
+
+    kwargs = collections.OrderedDict([(k, kwargs[k]) for k in argsname])
+    module = _class(**kwargs)
+
+    if config.reset_inline or config.reset_file:
+        _code = config.reset_inline if config.reset_inline \
+                                    else open(config.reset_file, 'r').read()
+        exec( _code )
+
+    return locals()['module']
+    
+    
+
+
+# parse optimizer
+def _parse_optimizer(config, model, classes=torch.optim):
+    '''parse optimizer config
+    '''
+    if config.module_file or config.module_inline:
+        _code = config.module_inline if config.module_inline else open(config.module_file, 'r').read()
+        exec( _code )            
+        return locals()['optimizer']
+
+    _class = getattr(classes, config.type)
+
+    argspec = inspect.getfullargspec(_class.__init__)
+    argsname = [arg for arg in argspec.args if arg != 'self']
+
+    kwargs = {}
+
+    if argspec.defaults is not None:
+        kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
+
+    if len(config.params_group):
+        params = []
+        for group in config.params_group:
+            exec(group.params_inline)
+            _var_name = group.params_inline.split('=')[0].strip()                
+            _params = {k.name:v for k, v in group.ListFields() if k.name != 'params_inline'}
+            _params.update({'params': locals()[_var_name]})
+            params.append(_params)
+    else:
+        params = model.parameters()
+
+    kwargs.update( {'params': params})
+
+    _param = {k.name: v for k, v in config.ListFields()}
+    kwargs.update({k: _param[k] for k in argsname if k in _param})
+
+    _optimizer = _class( **kwargs )
+
+    return _optimizer
+
+
+    
+# parse lr scheduler
+def _parse_lr_scheduler(config, optimizer, classes=torch.optim.lr_scheduler):
+    '''parse lr_scheduler config
+    '''
+    if config.module_file or config.module_inline:
+        _code = config.module_inline if config.module_inline else open(config.module_file, 'r').read()
+        exec( _code )            
+        return locals()['lr_scheduler']
+
+    _class = getattr(classes, config.type)
+
+    argspec = inspect.getfullargspec(_class.__init__)
+    argsname = [arg for arg in argspec.args if arg != 'self']
+
+    kwargs = {}
+
+    if argspec.defaults is not None:
+        kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
+
+    _param = {k.name: v for k, v in config.ListFields()}
+    kwargs.update({k: _param[k] for k in argsname if k in _param})
+
+    kwargs.update( {'optimizer': optimizer} )
+
+    _lr_scheduler = _class( **kwargs )
+
+    return _lr_scheduler    
+
+
+# parse dataloader
+def _parse_dataloader(config, dataset, ):
+    '''parse dataloader
+    '''
+    if config.module_file or config.module_inline:
+        _code = config.module_inline if config.module_inline else open(config.module_file, 'r').read()
+        exec( _code )            
+        return locals()['dataloader']
+
+    _class = torch.utils.data.DataLoader
+
+    argspec = inspect.getfullargspec(_class.__init__)
+    argsname = [arg for arg in argspec.args if arg != 'self']
+
+    kwargs = {}
+
+    if argspec.defaults is not None:
+        kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
+
+    _param = {k.name: v for k, v in config.ListFields()}
+    kwargs.update({k: _param[k] for k in argsname if k in _param})
+
+    kwargs.update( {'dataset': dataset} )
+
+    _dataloader = _class( **kwargs )
+
+    return _dataloader
+    
+    
+    
     
 class Model(nn.Module):
     def __init__(self, model_param=None, model_file='./pp_model.prototxt'):
@@ -58,54 +204,14 @@ class Model(nn.Module):
         
         for i, m in enumerate(config.module):
             _name = m.name
-            _module = self._parse_module(m, MODULES)
-            
+            _module = _parse_module(m, MODULES)
+
             modules.append( _module )
             
         return modules
     
     
-    @staticmethod
-    def _parse_module(config, classes):
-        '''instantiate a module
-        '''
-        if config.type == 'Custom':
-            _code = config.custom_param.module_inline if config.custom_param.module_inline \
-                else open(config.custom_param.module_file, 'r').read()
-            exec(_code)
-            
-            return locals()[config.name]
-            
-        _type = config.type
-        _class = classes[_type]
-        
-        argspec = inspect.getfullargspec(_class.__init__)
-        argsname = [arg for arg in argspec.args if arg != 'self']
 
-        kwargs = {}
-
-        if argspec.defaults is not None:
-            kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
-
-        if hasattr(config, f'{_type.lower()}_param'):
-            _param = getattr(config, f'{_type.lower()}_param')
-            _param = {k.name: v for k, v in _param.ListFields()}
-            _param.update({k: (list(v)[0] if len(v) == 1 else list(v)) \
-                          for k, v in _param.items() if isinstance(v, pyext._message.RepeatedScalarContainer)})
-            
-            kwargs.update({k: _param[k] for k in argsname if k in _param})
-
-        kwargs = collections.OrderedDict([(k, kwargs[k]) for k in argsname])
-        module = _class(**kwargs)
-        
-        if config.reset_inline or config.reset_file:
-            _code = config.reset_inline if config.reset_inline \
-                                        else open(config.reset_file, 'r').read()
-            exec( _code )
-        
-        return locals()['module']
-        
-        
 
 # model = Model()
 # model = Model()
@@ -121,34 +227,32 @@ class Solver(object):
         text_format.Merge(open(solver_file, 'rb').read(), solver_param)
 
         model = Model(solver_param.model, solver_param.model_file)
-        # self.model = model
+        self.model = model
         
         if solver_param.optimizer.ByteSize():
-            optimizer = self.parse_optimizer(solver_param.optimizer, model)
-            # optimizer = self.parse(solver_param.optimizer, torch.optim)
-            # self.optimizer = optimizer
+            optimizer = _parse_optimizer(solver_param.optimizer, model)
+            self.optimizer = optimizer
             
         if solver_param.lr_scheduler.ByteSize():
-            lr_scheduler = self.parse_lr_scheduler(solver_param.lr_scheduler, optimizer)
-            # lr_scheduler = self.parse(solver_param.lr_scheduler, torch.optim.lr_scheduler)
-            # self.lr_scheduler = lr_scheduler
-        
-        self.model = model
-        self.optimizer = optimizer
-        self.lr_scheduler = lr_scheduler
-        self.dataloader = None
+            lr_scheduler = _parse_lr_scheduler(solver_param.lr_scheduler, optimizer)
+            self.lr_scheduler = lr_scheduler
+            
+        if len(solver_param.dataset):
+            dataset = {_m.name: _parse_module(_m, MODULES) for _m in solver_param.dataset}
 
+        if len(solver_param.dataloader):
+            dataloader = {_m.name: _parse_dataloader(_m, dataset[_m.dataset]) for _m in solver_param.dataloader}
+            self.dataloader = dataloader
+        
+        
         if solver_param.distributed.ByteSize():
             self.setup_distributed(solver_param.distributed)
         else:
             self.device = torch.device(solver_param.device)
-            self.model = model.to(self.device)
+            self.model = self.model.to(self.device)
         
         self.last_epoch = 0
         self.epoches = solver_param.epoches
-        
-        print(solver_param)
-        
         
         
     def train(self, ):
@@ -187,77 +291,9 @@ class Solver(object):
         self.optimizer.load_state_dict(state['optimizer'])
         self.model.load_state_dict(state['lr_scheduler'])
         self.last_epoch = state['last_epoch']
-
-
-    @staticmethod
-    def parse_optimizer(config, model, classes=torch.optim):
-        '''parse optimizer config
-        '''
-        if config.module_file or config.module_inline:
-            _code = config.module_inline if config.module_inline else open(config.module_file, 'r').read()
-            exec( _code )            
-            return locals()['optimizer']
-        
-        _class = getattr(classes, config.type)
-        
-        argspec = inspect.getfullargspec(_class.__init__)
-        argsname = [arg for arg in argspec.args if arg != 'self']
-
-        kwargs = {}
-
-        if argspec.defaults is not None:
-            kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
-        
-        if len(config.params_group):
-            params = []
-            for group in config.params_group:
-                exec(group.params_inline)
-                _var_name = group.params_inline.split('=')[0].strip()                
-                _params = {k.name:v for k, v in group.ListFields() if k.name != 'params_inline'}
-                _params.update({'params': locals()[_var_name]})
-                params.append(_params)
-        else:
-            params = model.parameters()
-        
-        kwargs.update( {'params': params})
-        
-        _param = {k.name: v for k, v in config.ListFields()}
-        kwargs.update({k: _param[k] for k in argsname if k in _param})
-        
-        _optimizer = _class( **kwargs )
-        
-        return _optimizer
-
     
-    @staticmethod
-    def parse_lr_scheduler(config, optimizer, classes=torch.optim.lr_scheduler):
-        '''parse lr_scheduler config
-        '''
-        if config.module_file or config.module_inline:
-            _code = config.module_inline if config.module_inline else open(config.module_file, 'r').read()
-            exec( _code )            
-            return locals()['lr_scheduler']
-        
-        _class = getattr(classes, config.type)
-        
-        argspec = inspect.getfullargspec(_class.__init__)
-        argsname = [arg for arg in argspec.args if arg != 'self']
-
-        kwargs = {}
-
-        if argspec.defaults is not None:
-            kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
-
-        kwargs.update( {'optimizer': optimizer} )
-
-        _param = {k.name: v for k, v in config.ListFields()}
-        kwargs.update({k: _param[k] for k in argsname if k in _param})
-        
-        _lr_scheduler = _class( **kwargs )
-        
-        return _lr_scheduler    
-
-
+    
+    
     
     def setup_distributed(self, config):
         '''distributed setup
@@ -265,8 +301,9 @@ class Solver(object):
         '''
         dist.init_process_group(backend = (config.backend if config.backend else 'nccl'),
                                 init_method = (config.init_method if config.init_method else 'env://' ), 
-                                # world_size = args.world_size, 
-                                rank = args.local_rank )
+                                # world_size = (config.world_size if config.init_method else args.world_size), 
+                                # rank = args.local_rank 
+                               )
         
         torch.cuda.set_device(args.local_rank)
         
@@ -302,6 +339,8 @@ class Solver(object):
         __builtin__.print = print
 
 
+        
+        
 if __name__ == '__main__':
     
     # python -m torch.distributed.launch --nproc_per_node=2 pptest.py
