4d9fc683b7c645f94d3dee7c57349c02ad2f1e11
lyuwenyu <wenyu.lyu@gmail.com>
2021-07-24 15:22:34 +0800

    update

--- pp/protobuf/pp_utils.py
+++ pp/protobuf/pp_utils.py
@@ -3,20 +3,31 @@
 from collections import OrderedDict
 from typing import Dict, Any
 
+from google.protobuf import pyext
 
 
 
-def get_module_param(config):
+
+def get_module_param(config, with_type=True):
+    '''get_module_param
+    '''
     _name = [k.name for k, _ in config.ListFields() 
              if '_param' in k.name and f'{_type.lower()}param' == k.name.replace('_', '')]
     assert len(_name) <= 1, ''
     
     params = {}
-    if len(_name) == 1:
+    if len(_name) == 1 and use_type:
         _param = getattr(config, _name[0])
+        _param = {k.name: v for k, v in _param.ListFields()}
         
+        for k, v in _param.items():
+            if isinstance(v, pyext._message.RepeatedScalarContainer):
+                v = list(v)
+                v = v[0] if len(v) == 1 and k in ('kernel_size', 'stride', 'padding') else v
+            
+            params[k] = v
+                
     return params
-    
         
     
 def build_module(config, modules: Dict[str, Any]):
@@ -37,7 +48,7 @@ def build_module(config, modules: Dict[str, Any]):
                 value: 0.1
             }
         }
-    }   
+    }
     '''
     _type = config.type
     _clss = modules[_type]
@@ -49,9 +60,7 @@ def build_module(config, modules: Dict[str, Any]):
     if argspec.defaults is not None:
         kwargs.update( dict(zip(argsname[::-1], argspec.defaults[::-1])) )
 
-    # {k: v for k, v in get_module_param(config).items() if k in ('kernel_size', 'stride', 'padding', 'dilation') and len(v)==1}
-    
-    kwargs.update()
+    kwargs.update(get_module_param(config))
     
     kwargs = OrderedDict([(k, kwargs[k]) for k in argsname])
     module = _class(**kwargs)
@@ -59,3 +68,10 @@ def build_module(config, modules: Dict[str, Any]):
     config['module'] = module
     
     return module
+
+
+
+if __name__ == '__main__':
+    
+    
+    
