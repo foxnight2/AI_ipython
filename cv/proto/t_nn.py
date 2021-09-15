ed3ebe41644ceb65c869482b7f887b687fed8c35
lyuwenyu <wenyu.lyu@gmail.com>
2021-09-15 16:01:40 +0800

    update

---
+++ cv/proto/t_nn.py
@@ -0,0 +1,44 @@
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from typing import Sequence
+
+
+class ConvNorm2d(torch.nn.Module):
+    def __init__(self, ):
+        super().__init__()
+
+    def forward(self, x):
+        pass
+    
+
+def _to_list(x, n):
+    '''
+    '''
+    if isinstance(x, Sequence):
+        return list(x)
+    else:
+        return [x, ] * n
+
+
+class MultiScaleResize(nn.Module):
+    '''MultiScaleResize
+    '''
+    def __init__(self, size):
+        super().__init__()
+        self.size = _to_list(size, 1)
+
+    def forward(self, x):
+        _idx = torch.randint(len(self.size), (1, )).item()
+        return F.interpolate(x, self.size[_idx])
+
+
+
+class Detach(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        return x.detach()
